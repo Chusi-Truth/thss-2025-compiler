6ae171205d1c22b2aeabc655b873b49825a5a9c4
Xujie Han <1498126985@qq.com>
2025-10-28 19:36:46 +0800

    <pa3>: it runs

--- pa3/include/SysYFormatter.h
+++ pa3/include/SysYFormatter.h
@@ -1,3 +1,81 @@
 #pragma once
-
+#include <any>
+#include <string>
+#include "./SysYParserBaseVisitor.h"
 // TODO: Implement SysYFormatter
+
+class SysYFormatter : public SysYParserBaseVisitor
+{
+public:
+    virtual std::any visitCompUnit(SysYParser::CompUnitContext *ctx) override;
+
+    virtual std::any visitDecl(SysYParser::DeclContext *ctx) override;
+    virtual std::any visitConstDecl(SysYParser::ConstDeclContext *ctx) override;
+
+    virtual std::any visitBType(SysYParser::BTypeContext *ctx) override;
+
+    virtual std::any visitConstDef(SysYParser::ConstDefContext *ctx) override;
+
+    virtual std::any visitConstInitVal(SysYParser::ConstInitValContext *ctx) override;
+
+    virtual std::any visitVarDecl(SysYParser::VarDeclContext *ctx) override;
+    virtual std::any visitVarDef(SysYParser::VarDefContext *ctx) override;
+
+    virtual std::any visitInitVal(SysYParser::InitValContext *ctx) override;
+
+    virtual std::any visitFuncDef(SysYParser::FuncDefContext *ctx) override;
+
+    virtual std::any visitFuncType(SysYParser::FuncTypeContext *ctx) override;
+
+    virtual std::any visitFuncFParams(SysYParser::FuncFParamsContext *ctx) override;
+
+    virtual std::any visitFuncFParam(SysYParser::FuncFParamContext *ctx) override
+    ;
+
+    virtual std::any visitBlock(SysYParser::BlockContext *ctx) override;
+
+    virtual std::any visitBlockItem(SysYParser::BlockItemContext *ctx) override;
+
+    virtual std::any visitStmt(SysYParser::StmtContext *ctx) override;
+
+    virtual std::any visitExp(SysYParser::ExpContext *ctx) override;
+
+    virtual std::any visitCond(SysYParser::CondContext *ctx) override;
+
+    virtual std::any visitLVal(SysYParser::LValContext *ctx) override;
+
+    virtual std::any visitPrimaryExp(SysYParser::PrimaryExpContext *ctx) override;
+
+    virtual std::any visitNumber(SysYParser::NumberContext *ctx) override;
+
+    virtual std::any visitUnaryExp(SysYParser::UnaryExpContext *ctx) override;
+
+    virtual std::any visitUnaryOp(SysYParser::UnaryOpContext *ctx) override;
+
+    virtual std::any visitFuncRParams(SysYParser::FuncRParamsContext *ctx) override;
+
+    virtual std::any visitMulExp(SysYParser::MulExpContext *ctx);
+
+    virtual std::any visitAddExp(SysYParser::AddExpContext *ctx);
+
+    virtual std::any visitRelExp(SysYParser::RelExpContext *ctx);
+
+    virtual std::any visitEqExp(SysYParser::EqExpContext *ctx) override;
+
+    virtual std::any visitLAndExp(SysYParser::LAndExpContext *ctx) override;
+
+    virtual std::any visitLOrExp(SysYParser::LOrExpContext *ctx) override;
+
+    virtual std::any visitConstExp(SysYParser::ConstExpContext *ctx) override;
+
+private:
+    // 当前缩进层级（
+    int indent = 0;
+    // 是否已输出过第一个函数，用于在函数之间插入空行
+    bool firstFunctionSeen = false;
+
+    // 返回当前缩进字符串
+    std::string indentStr() const;
+    // 去掉字符串行尾多余空格
+    std::string trimTrailing(const std::string &s) const;
+};
--- pa3/src/SysYFormatter.cpp
+++ pa3/src/SysYFormatter.cpp
@@ -1,3 +1,67 @@
 #include "SysYFormatter.h"
+#include <sstream>
+#include <algorithm>
+#include <cctype>
+using namespace std;
 
-// TODO: Implement specific visitXXX methods.
+std::any SysYFormatter::visitCompUnit(SysYParser::CompUnitContext *ctx)
+{
+    return string("111");
+}
+std::any SysYFormatter::visitDecl(SysYParser::DeclContext *ctx) {};
+std::any SysYFormatter::visitConstDecl(SysYParser::ConstDeclContext *ctx) {};
+
+std::any SysYFormatter::visitBType(SysYParser::BTypeContext *ctx) {};
+
+std::any SysYFormatter::visitConstDef(SysYParser::ConstDefContext *ctx) {};
+
+std::any SysYFormatter::visitConstInitVal(SysYParser::ConstInitValContext *ctx) {};
+
+std::any SysYFormatter::visitVarDecl(SysYParser::VarDeclContext *ctx) {};
+std::any SysYFormatter::visitVarDef(SysYParser::VarDefContext *ctx) {};
+
+std::any SysYFormatter::visitInitVal(SysYParser::InitValContext *ctx) {};
+
+std::any SysYFormatter::visitFuncDef(SysYParser::FuncDefContext *ctx) {};
+
+std::any SysYFormatter::visitFuncType(SysYParser::FuncTypeContext *ctx) {};
+
+std::any SysYFormatter::visitFuncFParams(SysYParser::FuncFParamsContext *ctx) {};
+
+std::any SysYFormatter::visitFuncFParam(SysYParser::FuncFParamContext *ctx) {};
+
+std::any SysYFormatter::visitBlock(SysYParser::BlockContext *ctx) {};
+
+std::any SysYFormatter::visitBlockItem(SysYParser::BlockItemContext *ctx) {};
+
+std::any SysYFormatter::visitStmt(SysYParser::StmtContext *ctx) {};
+
+std::any SysYFormatter::visitExp(SysYParser::ExpContext *ctx) {};
+
+std::any SysYFormatter::visitCond(SysYParser::CondContext *ctx) {};
+
+std::any SysYFormatter::visitLVal(SysYParser::LValContext *ctx) {};
+
+std::any SysYFormatter::visitPrimaryExp(SysYParser::PrimaryExpContext *ctx) {};
+
+std::any SysYFormatter::visitNumber(SysYParser::NumberContext *ctx) {};
+
+std::any SysYFormatter::visitUnaryExp(SysYParser::UnaryExpContext *ctx) {};
+
+std::any SysYFormatter::visitUnaryOp(SysYParser::UnaryOpContext *ctx) {};
+
+std::any SysYFormatter::visitFuncRParams(SysYParser::FuncRParamsContext *ctx) {};
+
+std::any SysYFormatter::visitMulExp(SysYParser::MulExpContext *ctx){};
+
+std::any SysYFormatter::visitAddExp(SysYParser::AddExpContext *ctx){};
+
+std::any SysYFormatter::visitRelExp(SysYParser::RelExpContext *ctx){};
+
+std::any SysYFormatter::visitEqExp(SysYParser::EqExpContext *ctx) {};
+
+std::any SysYFormatter::visitLAndExp(SysYParser::LAndExpContext *ctx) {};
+
+std::any SysYFormatter::visitLOrExp(SysYParser::LOrExpContext *ctx) {};
+
+std::any SysYFormatter::visitConstExp(SysYParser::ConstExpContext *ctx) {};
