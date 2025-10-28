b8af90a9cce82168a8102f34403c2a4ff9815fde
Xujie Han <1498126985@qq.com>
2025-10-28 19:44:08 +0800

    <pa3>: add tips

--- pa3/src/SysYFormatter.cpp
+++ pa3/src/SysYFormatter.cpp
@@ -4,64 +4,84 @@
 #include <cctype>
 using namespace std;
 
+/*
+note:需要讨论的问题
+换行：
+1.有分号必换行
+2.有大代码块必换行
+
+缩进：\n 后必加上缩进
+
+*/
+
+
+
+
+
+
 std::any SysYFormatter::visitCompUnit(SysYParser::CompUnitContext *ctx)
 {
-    return string("111");
+    string out;
+    for(auto obj : ctx->children)
+    {
+
+    }
+    
 }
-std::any SysYFormatter::visitDecl(SysYParser::DeclContext *ctx) {};
-std::any SysYFormatter::visitConstDecl(SysYParser::ConstDeclContext *ctx) {};
+std::any SysYFormatter::visitDecl(SysYParser::DeclContext *ctx) {return ctx->getText();};
+std::any SysYFormatter::visitConstDecl(SysYParser::ConstDeclContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitBType(SysYParser::BTypeContext *ctx) {};
+std::any SysYFormatter::visitBType(SysYParser::BTypeContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitConstDef(SysYParser::ConstDefContext *ctx) {};
+std::any SysYFormatter::visitConstDef(SysYParser::ConstDefContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitConstInitVal(SysYParser::ConstInitValContext *ctx) {};
+std::any SysYFormatter::visitConstInitVal(SysYParser::ConstInitValContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitVarDecl(SysYParser::VarDeclContext *ctx) {};
-std::any SysYFormatter::visitVarDef(SysYParser::VarDefContext *ctx) {};
+std::any SysYFormatter::visitVarDecl(SysYParser::VarDeclContext *ctx) {return ctx->getText();};
+std::any SysYFormatter::visitVarDef(SysYParser::VarDefContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitInitVal(SysYParser::InitValContext *ctx) {};
+std::any SysYFormatter::visitInitVal(SysYParser::InitValContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitFuncDef(SysYParser::FuncDefContext *ctx) {};
+std::any SysYFormatter::visitFuncDef(SysYParser::FuncDefContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitFuncType(SysYParser::FuncTypeContext *ctx) {};
+std::any SysYFormatter::visitFuncType(SysYParser::FuncTypeContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitFuncFParams(SysYParser::FuncFParamsContext *ctx) {};
+std::any SysYFormatter::visitFuncFParams(SysYParser::FuncFParamsContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitFuncFParam(SysYParser::FuncFParamContext *ctx) {};
+std::any SysYFormatter::visitFuncFParam(SysYParser::FuncFParamContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitBlock(SysYParser::BlockContext *ctx) {};
+std::any SysYFormatter::visitBlock(SysYParser::BlockContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitBlockItem(SysYParser::BlockItemContext *ctx) {};
+std::any SysYFormatter::visitBlockItem(SysYParser::BlockItemContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitStmt(SysYParser::StmtContext *ctx) {};
+std::any SysYFormatter::visitStmt(SysYParser::StmtContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitExp(SysYParser::ExpContext *ctx) {};
+std::any SysYFormatter::visitExp(SysYParser::ExpContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitCond(SysYParser::CondContext *ctx) {};
+std::any SysYFormatter::visitCond(SysYParser::CondContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitLVal(SysYParser::LValContext *ctx) {};
+std::any SysYFormatter::visitLVal(SysYParser::LValContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitPrimaryExp(SysYParser::PrimaryExpContext *ctx) {};
+std::any SysYFormatter::visitPrimaryExp(SysYParser::PrimaryExpContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitNumber(SysYParser::NumberContext *ctx) {};
+std::any SysYFormatter::visitNumber(SysYParser::NumberContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitUnaryExp(SysYParser::UnaryExpContext *ctx) {};
+std::any SysYFormatter::visitUnaryExp(SysYParser::UnaryExpContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitUnaryOp(SysYParser::UnaryOpContext *ctx) {};
+std::any SysYFormatter::visitUnaryOp(SysYParser::UnaryOpContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitFuncRParams(SysYParser::FuncRParamsContext *ctx) {};
+std::any SysYFormatter::visitFuncRParams(SysYParser::FuncRParamsContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitMulExp(SysYParser::MulExpContext *ctx){};
+std::any SysYFormatter::visitMulExp(SysYParser::MulExpContext *ctx){return ctx->getText();};
 
-std::any SysYFormatter::visitAddExp(SysYParser::AddExpContext *ctx){};
+std::any SysYFormatter::visitAddExp(SysYParser::AddExpContext *ctx){return ctx->getText();};
 
-std::any SysYFormatter::visitRelExp(SysYParser::RelExpContext *ctx){};
+std::any SysYFormatter::visitRelExp(SysYParser::RelExpContext *ctx){return ctx->getText();};
 
-std::any SysYFormatter::visitEqExp(SysYParser::EqExpContext *ctx) {};
+std::any SysYFormatter::visitEqExp(SysYParser::EqExpContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitLAndExp(SysYParser::LAndExpContext *ctx) {};
+std::any SysYFormatter::visitLAndExp(SysYParser::LAndExpContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitLOrExp(SysYParser::LOrExpContext *ctx) {};
+std::any SysYFormatter::visitLOrExp(SysYParser::LOrExpContext *ctx) {return ctx->getText();};
 
-std::any SysYFormatter::visitConstExp(SysYParser::ConstExpContext *ctx) {};
+std::any SysYFormatter::visitConstExp(SysYParser::ConstExpContext *ctx) {return ctx->getText();};
